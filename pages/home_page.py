4c92bbce0232399a5fc6f6de3787dc5d602553d0
JustSzat <justyna.salek123@gmail.com>
2026-04-13 11:59:08 +0200

    Added time sleep and fixed enetering to home page by LINK.TEXT

--- pages/home_page.py
+++ pages/home_page.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium.webdriver.support import expected_conditions as EC
 
 from selenium.webdriver.common.by import By
@@ -9,5 +11,6 @@ from pages.base_page import BasePage
 class HomePage(BasePage):
 
     def click_enter_the_store(self):
-        enter_click = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID, "Content")))
+        enter_click = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.LINK_TEXT, 'Enter the Store')))
         enter_click.click()
+        time.sleep(6)
